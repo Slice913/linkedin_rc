0d2553b775de1ffc5c88ea5d9017df2e98d9095a
Slice913 <akeemayoade@gmail.com>
2022-05-23 15:33:57 -0500

    ADD: Header content inprogress

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import Login from './components/Login';
+import Header from './components/Header';
+import Home from './components/Home';
 import {
   BrowserRouter,
   Routes,
   Route,
 } from "react-router-dom";
+
 
 function App() {
   return (
@@ -13,6 +16,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login/>}/>
+          <Route path="/home" element={<><Header/> <Home/></>}/>
         </Routes>
       </BrowserRouter>
     </div>
